efd805d150f3588c48937987ac7709eb2f7f5195
Thiago Kichiro <kichiro.88@gmail.com>
2020-08-25 00:02:34 -0300

    desafio frontend finalizado

--- src/App.js
+++ src/App.js
@@ -23,7 +23,10 @@ function App() {
   async function handleRemoveRepository(id) {
     await api.delete(`repositories/${id}`);
 
-    setRepositories(repositories.filter((repository) => repository.id !== id));
+    const newRepository = repositories.filter(
+      (repository) => repository.id !== id
+    );
+    setRepositories(newRepository);
   }
 
   return (
